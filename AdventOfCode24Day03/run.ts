78f73190a077f1ba42e2baabdf1cdad2ca1f30c5
William Sullivan <william@symbotics.co.uk>
2024-12-03 00:30:21 -0500

    Day 3 - Part 2

--- AdventOfCode24Day03/run.ts
+++ AdventOfCode24Day03/run.ts
@@ -1,6 +1,6 @@
 import { getInput } from "../inputs/getInput";
 
-var example = true;
+var example = false;
 
 var data = getInput(3, example);
 
@@ -16,3 +16,17 @@ var values = matches?.map(matchToVal);
 var sum = values?.reduce((x, y) => x + y);
 
 console.log(`Sum of Multiplications: ${sum}`);
+
+var matches = data.match(/mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)/g);
+var doing = true;
+var sumBetter  = matches?.reduce((sum, match) => {
+    if (match == 'do()')
+        doing = true;
+    else if (match == 'don\'t()')
+        doing = false;
+    else if (doing)
+        return sum + matchToVal(match);
+    return sum;
+}, 0);
+
+console.log(`Better sum of Multiplications: ${sumBetter}`);
