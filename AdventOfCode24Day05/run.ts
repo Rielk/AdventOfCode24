349f5b14368de49e2a17d7b477a47c859cfba3e9
William Sullivan <william@symbotics.co.uk>
2024-12-05 02:02:19 -0500

    Day 5 - Cleanup

--- AdventOfCode24Day05/run.ts
+++ AdventOfCode24Day05/run.ts
@@ -3,16 +3,13 @@ import { processInput } from "../inputs/getInput";
 
 var example = false;
 
-var rules = new Map<number, Array<number>>();
+var rules = new Map<number, number[]>();
 var updates: number[][] = [];
 processInput(function (line: string) {
-    if (line == '') {
+    if (line == '') 
         this.part2 = true;
-        return;
-    }
-    if (this.part2){
+    else if (this.part2)
         updates.push(line.split(',').map(s => parseInt(s)));
-    }
     else {
         var [before, after] = line.split('|').map(s => parseInt(s));
         var existing = rules.get(after);
@@ -23,68 +20,59 @@ processInput(function (line: string) {
     }
 }, 5, example, {});
 
-var passingUpdates = updates.filter(update => checkOrder(update));
-var sumOfMiddlePassing = passingUpdates.map(update => update[Math.floor(update.length/2)]).reduce((x,y) => x+y);
+var { pass: passingUpdates, fail: failingUpdates } = updates.reduce(({ pass, fail }: { pass: number[][], fail: number[][] }, update) => {
+    checkOrder(update) ? pass.push(update) : fail.push(update);
+    return { pass, fail };
+}, { pass: [], fail: [] });
 
-var failingUpdates = updates.filter(update => !checkOrder(update));
 var fixedUpdates = failingUpdates.map(update => fixOrder(update));
-var sumOfMiddleFixed = fixedUpdates.map(update => update[Math.floor(update.length/2)]).reduce((x,y) => x+y);
+
+var sumOfMiddlePassing = sumMiddleValues(passingUpdates);
+var sumOfMiddleFixed = sumMiddleValues(fixedUpdates);
 
 function checkOrder(update: number[]) : boolean {
-    for (let i = 0; i < update.length; i++) {
-        const page = update[i];
-        const required = rules.get(page);
-        if (!hasRequiredPagesBefore(required, i))
+    for (let i = 0; i < update.length; i++)
+        if (!hasRequiredPagesBefore(rules.get(update[i]), i))
             return false;
-    }
     return true;
 
     function hasRequiredPagesBefore(required: number[] | undefined, i: number) : boolean {
         if (required == undefined || required.length <= 0)
             return true;
 
-        for (let ri = 0; ri < required.length; ri++) {
-            const reqPage = required[ri];
-            for (let ci = i; ci < update.length; ci++) {
-                const checkPage = update[ci];
-                if (reqPage == checkPage)
+        for (let ri = 0; ri < required.length; ri++) 
+            for (let ci = i; ci < update.length; ci++) 
+                if (required[ri] == update[ci])
                     return false;
-            }
-        }
         return true;
     }
 }
 
 function fixOrder(update: number[]) : number[] {
     update = [...update];
-
-    for (let i = 0; i < update.length; i++) {
-        const page = update[i];
-        const required = rules.get(page);
-        if(moveRequiredPages(required, i))
+    for (let i = 0; i < update.length; i++) 
+        if(moveRequiredPages(rules.get(update[i]), i))
             i--;
-    }
+    return update;
 
-    function moveRequiredPages(required: number[] | undefined, i: number) : boolean {
+    function moveRequiredPages(required: number[] | undefined, ii: number) : boolean {
         if (required == undefined || required.length <= 0)
             return false;
 
         var changed = false;
-        for (let ri = 0; ri < required.length; ri++) {
-            const reqPage = required[ri];
-            for (let ci = i; ci < update.length; ci++) {
-                const checkPage = update[ci];
-                if (reqPage == checkPage) {
-                    arrayMove(update, ci, i);
+        for (let ri = 0; ri < required.length; ri++)
+            for (let ui = ii; ui < update.length; ui++)
+                if (required[ri] == update[ui]) {
+                    arrayMove(update, ui, ii);
                     changed = true;
                 }
-            }
-        }
         return changed;
     }
-
-    return update;
 }
 
-console.log(sumOfMiddlePassing);
-console.log(sumOfMiddleFixed);
+function sumMiddleValues(updates: number[][]): number {
+    return updates.map(update => update[Math.floor(update.length/2)]).reduce((x,y) => x+y)
+}
+
+console.log(`Sum of Middle of Correctly-Ordered updates: ${sumOfMiddlePassing}`);
+console.log(`Sum of Middle of Fixed updates: ${sumOfMiddleFixed}`);
