0c9f4e5e34134079bd48c88c07e272ff53493521
William Sullivan <william@symbotics.co.uk>
2024-12-09 02:52:28 -0500

    Day 9 - Part 1

--- AdventOfCode24Day09/run.ts
+++ AdventOfCode24Day09/run.ts
@@ -1,5 +1,40 @@
 import { getInput } from "../inputs/getInput";
 
-var example = false;
+var example = true;
 
-var data = getInput(9, example);
+var data : (number | undefined)[]= [];
+{
+    var empty = false;
+    var input = getInput(9, example);
+    var id = 0;
+    for (let n = 0; n < input.length; n++) {
+        const number = parseInt(input[n]);
+        if (empty)
+            for (let i = 0; i < number; i++)
+                data.push(undefined);
+        else {
+            for (let i = 0; i < number; i++)
+                data.push(id)
+            id++;
+        }
+        empty = !empty;
+    }
+}
+
+var checksum = calcChecksum(data);
+
+function calcChecksum(data: (number | undefined)[]): number {
+    data = [...data];
+    var ret = 0;
+    for (let i = 0; i < data.length; i++) {
+        var element = data[i];
+        while (element == undefined && i < data.length)
+            element = data.pop();
+        if (element == undefined)
+            break;
+        ret += i * element;
+    }
+    return ret;
+}
+
+console.log(`Checksum: ${checksum}`)
