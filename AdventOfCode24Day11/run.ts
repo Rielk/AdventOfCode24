776b07f8ed6e99bf238d6325814a84e211c9fb38
Rielk <william@symbotics.co.uk>
2024-12-12 10:21:27 +0000

    Day 11 - Part 2

--- AdventOfCode24Day11/run.ts
+++ AdventOfCode24Day11/run.ts
@@ -1,33 +1,50 @@
+import { time } from "node:console";
 import { getInput } from "../inputs/getInput";
 
 var example = false;
 
 var initialStones = getInput(11, example).split(' ').map(s => parseInt(s));
 
-function blinkNTimes(initialStones: number[], times: number) {
-    var finalStones: number[] = [];
-    for (let stone of initialStones)
-        addStoneEnding(stone, times, finalStones);
-    return finalStones;
+function calcStonesAfterNBlinks(initialStones: number[], times: number): number {
+    var count: number = 0;
+    var cache : Array<Map<number, number>> = new Array(times+1);
+    for (let i = 0; i < cache.length; i++)
+        cache[i] = new Map();
 
-    function addStoneEnding(stone: number, n: number, array: number[]): void {
-        if (n <= 0) {
-            array.push(stone);
-            return;
+    for (let stone of initialStones)
+        count += calcStone(stone, times);
+    return count;
+
+    function calcStone(stone: number, n: number): number {
+        if (n <= 0)
+            return 1;
+
+        let cachedValue  = cache[n].get(stone);
+        if (cachedValue != undefined)
+            return cachedValue;
+
+        if (stone == 0) {
+            let res = calcStone(1, n - 1)
+            cache[n].set(stone, res);
+            return res;
         }
-        if (stone == 0)
-            return addStoneEnding(1, n - 1, array);
         let order = Math.floor(Math.log10(stone)) + 1;
         if (order % 2 == 0) {
-            let magnitude = Math.pow(10, order/2);
+            let magnitude = Math.pow(10, order / 2);
             let right = stone % magnitude;
             let left = (stone - right) / magnitude;
-            addStoneEnding(left, n - 1, array);
-            addStoneEnding(right, n - 1, array);
-            return;
+            let res = calcStone(left, n - 1) + calcStone(right, n - 1);
+            cache[n].set(stone, res);
+            return res;
         }
-        return addStoneEnding(stone * 2024, n - 1, array);
+        let res = calcStone(stone * 2024, n - 1);
+        cache[n].set(stone, res);
+        return res;
     }
 }
 
-console.log(blinkNTimes(initialStones, 25).length);
+const result25 = calcStonesAfterNBlinks(initialStones, 25);
+const result75 = calcStonesAfterNBlinks(initialStones, 75);
+
+console.log(`Stones after 25 Blinks: ${result25}`);
+console.log(`Stones after 75 Blinks: ${result75}`);
