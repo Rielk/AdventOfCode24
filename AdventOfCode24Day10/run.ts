b4e75f93c06e61316e71d124a917d956383d77fe
Rielk <william@symbotics.co.uk>
2024-12-12 02:56:28 +0000

    Day 10 - Part 2

--- AdventOfCode24Day10/run.ts
+++ AdventOfCode24Day10/run.ts
@@ -8,38 +8,41 @@ var example = false;
     let inputs = mapInput((line) => {
         return line.split('').map(c => parseInt(c));
     }, 10, example);
-    var data = new Array2D<{ height: number, leadsTo: Set<Vector2> | undefined }>
+    var data = new Array2D<{ height: number, leadsTo: Vector2[] | undefined }>
         (inputs.length, inputs[0].length, (x, y) => { return { height: inputs[y][x], leadsTo: undefined } });
 }
-function followTrail(start: Vector2): Set<Vector2> {
+function followTrail(start: Vector2): Array<Vector2> {
     var thisLocData = data.getValue(start);
     if (thisLocData == undefined)
-        return new Set();
-
-    thisLocData.leadsTo = new Set();
-    if (thisLocData.height == 9) {
-         thisLocData.leadsTo.add(start);
-         return thisLocData.leadsTo;
-    }
+        return [];
+    if (thisLocData.height == 9)
+        return thisLocData.leadsTo = [start];
+    thisLocData.leadsTo = [];
     for (let adjacent of start.adjacent()) {
         let adjLocData = data.getValue(adjacent);
-        if (adjLocData == undefined ||  adjLocData.height != thisLocData.height + 1)
+        if (adjLocData == undefined || adjLocData.height != thisLocData.height + 1)
             continue;
         if (adjLocData.leadsTo == undefined)
             followTrail(adjacent);
-        adjLocData.leadsTo!.forEach(v => thisLocData!.leadsTo!.add(v));
+        thisLocData.leadsTo.splice(0,0, ...adjLocData.leadsTo!);
     }
     return thisLocData.leadsTo;
 }
 
-function calcScore(loc : Vector2) :number {
-    return followTrail(loc).size;
+function calcScore(loc : Vector2) : {score: number, rating: number} {
+    var trailHeads = followTrail(loc)
+    return {score: trailHeads.filter((v,i) => trailHeads.indexOf(v) == i).length,
+        rating: trailHeads.length
+    };
 }
 
-var score = 0;
+var score = 0, rating = 0;
 data.forEach2D((value, loc) => {
-    if (value.height == 0)
-        score += calcScore(new Vector2(loc.x, loc.y));
+    if (value.height == 0) {
+        var {score: s, rating :r} = calcScore(new Vector2(loc.x, loc.y));
+        score += s; rating += r;
+    }
 });
 
-console.log(score);
+console.log(`Sum of scores: ${score}`);
+console.log(`Sum of ratings: ${rating}`);
