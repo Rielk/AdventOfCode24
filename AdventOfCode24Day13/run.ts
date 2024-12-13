576b9e9967f12161d71736f66e4faf7c197ed4c7
Rielk <william@symbotics.co.uk>
2024-12-13 13:36:30 +0000

    Day 13 - Part 2

--- AdventOfCode24Day13/run.ts
+++ AdventOfCode24Day13/run.ts
@@ -5,7 +5,7 @@ var example = false;
 
 var data = mapInputLineBatches(batch => {
     var numbers = batch.map(line => (line.match(/\d+/g) ?? []).reduce((obj, value, i) => {
-        return { ...obj, [i == 0 ? 'x' : 'y']: value };
+        return { ...obj, [i == 0 ? 'x' : 'y']: parseInt(value) };
     }, { x: -1, y: -1 }));
     return {
         A: new Vector2(numbers[0].x, numbers[0].y),
@@ -14,8 +14,10 @@ var data = mapInputLineBatches(batch => {
     };
 }, 4, 13, example);
 
-function tokensForPrize(input: { A: Vector2, B: Vector2, prize: Vector2 }): number {
+function tokensForPrize(input: { A: Vector2, B: Vector2, prize: Vector2 }, prizeOffset?: number): number {
     let { A, B, prize } = input;
+    if (prizeOffset) 
+        prize = new Vector2(prize.x + prizeOffset, prize.y + prizeOffset); 
     // i*A.x + j*B.x = prize.x;  i*A.y + j*B.y = prize.y;
     // i = (prize.x - j*B.x) / A.x
     // (prize.x - j*B.x)*A.y + j*B.y*A.x = prize.y*A.x;
@@ -29,6 +31,8 @@ function tokensForPrize(input: { A: Vector2, B: Vector2, prize: Vector2 }): numb
     return 3 * i + j;
 }
 
-var tokens = data.map(v => tokensForPrize(v)).reduce((x, y) => x + y);
+var tokensWithoutOffset = data.map(v => tokensForPrize(v)).reduce((x, y) => x + y);
+var tokensWithOffset = data.map(v => tokensForPrize(v, 10000000000000)).reduce((x, y) => x + y);
 
-console.log(tokens);
+console.log(`Tokens Required for the Prizes: ${tokensWithoutOffset}`);
+console.log(`Tokens Required for the Prizes with offset: ${tokensWithOffset}`);
