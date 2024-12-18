116f924e019d58bd62e3110b59210813ce373c4f
Rielk <william@symbotics.co.uk>
2024-12-18 22:23:43 +0000

    Day 18 - Part 2

--- AdventOfCode24Day18/run.ts
+++ AdventOfCode24Day18/run.ts
@@ -2,7 +2,7 @@ import { Array2D } from "../general/array2D";
 import { runDijkstraAlgorithm } from "../general/Dijkstra/dijkstra";
 import { Vector2 } from "../general/vector2";
 import { mapInput } from "../inputs/getInput";
-import { State } from "./state";
+import { Node } from "./state";
 
 var example = false;
 
@@ -19,9 +19,32 @@ for (let i = 0; i < simCount; i++) {
     byteLocations.setValue(incomingBytes.shift()!, true);
 }
 
-let startState = new State(new Vector2(0, 0), byteLocations);
-let { weight } = runDijkstraAlgorithm(startState, n => {
-    return n.loc.equals(new Vector2(size-1, size-1));
-}) ?? {};
+function run(): { weight: number | undefined; finalNode: Node | undefined; } {
+    let startNode = new Node(new Vector2(0, 0), byteLocations);
+    let { weight, finalNode } = runDijkstraAlgorithm(startNode, n => {
+        return n.loc.equals(new Vector2(size - 1, size - 1));
+    }) ?? {};
+    return { weight: weight, finalNode: finalNode }
+}
+
+function findBreakByte() : Vector2 | undefined {
+    //This might run quicker if done using a binary search
+    //Adding half the remaining, then removing...
+    //But this works quick enough.
+    let { finalNode } = run();
+    while (incomingBytes.length > 0) {
+        let nextByte: Vector2 = incomingBytes.shift()!;
+        byteLocations.setValue(nextByte, true);
+        if (finalNode?.removeNodeFromPrevious(n => n.loc.equals(nextByte))) {
+            finalNode = run().finalNode;
+            if (finalNode == undefined)
+                return nextByte;
+        }
+    }
+}
+
+let { weight } = run();
+let breakByte = findBreakByte();
 
 console.log(weight);
+console.log(`${breakByte?.x},${breakByte?.y}`);
--- AdventOfCode24Day18/state.ts
+++ AdventOfCode24Day18/state.ts
@@ -2,10 +2,11 @@ import { Array2D } from "../general/array2D";
 import { iNode } from "../general/Dijkstra/iNode";
 import { Vector2 } from "../general/vector2";
 
-export class State implements iNode<State> {
+export class Node implements iNode<Node> {
     public uniqueId: number;
     public readonly loc: Vector2;
     public byteLocations: Array2D<boolean>;
+    private prevNodes: Node[] | undefined;
 
     constructor(loc: Vector2, byteLocations: Array2D<boolean>) {
         this.loc = loc;
@@ -13,15 +14,31 @@ export class State implements iNode<State> {
         this.byteLocations = byteLocations;
     }
 
-    *getNextNodes(): Generator<{ nextNode: State; addedWeight: number; }> {
+    *getNextNodes(): Generator<{ nextNode: Node; addedWeight: number; }> {
         for (let adj of this.loc.adjacent()) {
             if (this.byteLocations.getValue(adj) == false)
                 yield {
-                    nextNode: new State(adj, this.byteLocations),
+                    nextNode: new Node(adj, this.byteLocations),
                     addedWeight: 1
                 }
         }
     }
-    finalise(weight: number, prevNodes: State[]): void {}
+    finalise(weight: number, prevNodes: Node[]): void {
+        this.prevNodes = prevNodes;
+    }
 
+    removeNodeFromPrevious(predicate: (node:Node) => boolean) : boolean {
+        if (this.prevNodes == undefined)
+            return true;
+        let index = this.prevNodes.findIndex(predicate);
+        if (index >= 0)
+            this.prevNodes.splice(index, 1);
+        for (let i = 0; i < this.prevNodes.length; i++) {
+            if (this.prevNodes[i].removeNodeFromPrevious(predicate)) {
+                this.prevNodes.splice(i, 1);
+                i--;
+            }
+        }
+        return this.prevNodes.length <= 0;
+    }
 }
