9d8581a7e083628961295e0eb2337f8050236e1e
Rielk <william@symbotics.co.uk>
2024-12-19 10:27:57 +0000

    Day 19 - Part 1

---
+++ AdventOfCode24Day19/pattern.ts
@@ -0,0 +1,12 @@
+import { Towel } from "./towel";
+
+export class Pattern {
+    static beginsWith(pattern: string, towel: string): boolean {
+        if (towel.length > pattern.length)
+            return false;
+        for (let i = 0; i < towel.length; i++)
+            if (pattern[i] != towel[i])
+                return false;
+        return true;
+    }
+}
--- AdventOfCode24Day19/run.ts
+++ AdventOfCode24Day19/run.ts
@@ -1,5 +1,50 @@
-import { getInput } from "../inputs/getInput";
+import { map2SectionInput } from "../inputs/getInput";
+import { Pattern } from "./pattern";
 
 var example = false;
 
-var data = getInput(19, example);
+var { data1: towelArray, data2: patterns } = map2SectionInput(
+    line => {
+        return line.split(', ');
+    }, inputs => {
+        return inputs.flat();
+    }, line => {
+        return line;
+    }, x => x, 19, example);
+let towels = new Map<string, string[]>();
+towelArray.forEach(t => {
+    let start = t[0];
+    let arr = towels.get(start);
+    if (arr == undefined)
+        towels.set(start, arr = []);
+    arr.push(t);
+})
+
+function countMakeable(patterns: string[], towels: Map<string, string[]>): number {
+    let count = 0;
+    let cache = new Map<string, boolean>();
+    patterns.forEach(pattern => {
+        if (canMake(pattern, towels, cache))
+            count++;
+    });
+    return count;
+}
+
+function canMake(pattern: string, towels: Map<string, string[]>, cache: Map<string, boolean>): boolean {
+    let cached = cache.get(pattern);
+    if (cached != undefined)
+        return cached;
+    for (let newTowel of towels.get(pattern[0]) ?? []) {
+        if (!Pattern.beginsWith(pattern, newTowel))
+            continue;
+        let remainingPattern = pattern.slice(newTowel.length);
+        if (remainingPattern.length <= 0 || canMake(remainingPattern, towels, cache)){
+            cache.set(pattern, true);
+            return true;
+        }
+    }
+    cache.set(pattern, false);
+    return false;
+}
+
+console.log(countMakeable(patterns, towels));
