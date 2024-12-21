393b2496a3aac3fd4a6f0de6279925f62dab1ed3
Rielk <william@symbotics.co.uk>
2024-12-21 23:48:30 +0000

    Day 21 - Part 2

--- AdventOfCode24Day21/run.ts
+++ AdventOfCode24Day21/run.ts
@@ -1,5 +1,6 @@
 import { mapInput } from "../inputs/getInput";
 import { DirectionalKeypad } from "./directionalKeypad";
+import { iKeypad } from "./keypad";
 import { ManualKeypad } from "./manualKepad";
 import { NumericKeypad } from "./numericKeypad";
 
@@ -15,13 +16,24 @@ var targets = mapInput(line => {
     });
 }, 21, example);
 
-let numericKeypad = new NumericKeypad(new DirectionalKeypad(new DirectionalKeypad(new ManualKeypad())));
+function createKeypad(intermidiaries: number): NumericKeypad {
+    let keypad: iKeypad = new ManualKeypad();
+    for (let i = 0; i < intermidiaries; i++) {
+        keypad = new DirectionalKeypad(keypad);
+    }
+    return new NumericKeypad(keypad);
+}
+function calcTotalComplexities(keypad: NumericKeypad): number {
+    return targets.map(t => {
+        return {
+            length: keypad.leastInputsForCode(t),
+            numeric: parseInt(t.filter(x => x >= 0).join(''))
+        };
+    }).map(o => o.length * o.numeric).reduce((x, y) => x + y);
+}
 
-let complexities = targets.map(t => {
-    return {
-        length: numericKeypad.leastInputsForCode(t),
-        numeric: parseInt(t.filter(x => x >= 0).join(''))
-    };
-}).map(o => o.length * o.numeric);
+let numericKeypad2 = createKeypad(2);
+let numericKeypad25 = createKeypad(25);
 
-console.log(complexities.reduce((x, y) => x + y));
+console.log(`Total complexities with 2 directional robots: ${calcTotalComplexities(numericKeypad2)}`);
+console.log(`Total complexities with 25 directional robots: ${calcTotalComplexities(numericKeypad25)}`);
