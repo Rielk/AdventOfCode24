b70f12d26c17af59ca54f97246e11e171a08f7f7
Rielk <william@symbotics.co.uk>
2024-12-22 17:01:54 +0000

    Day 22 - Part 2 Slow

--- AdventOfCode24Day22/run.ts
+++ AdventOfCode24Day22/run.ts
@@ -1,10 +1,35 @@
 import { mapInput } from "../inputs/getInput";
-import { getSecretFrom } from "./secrets";
+import { genPriceChangeMapFrom, genSecretFrom, getChangesUID } from "./secrets";
 
 var example = false;
 
 var inputs = mapInput(line => parseInt(line), 22, example);
 
-let secrets2000 = inputs.map(i => getSecretFrom(i, 2000));
+let secrets2000 = inputs.map(i => genSecretFrom(i, 2000));
+let priceChangeMaps = inputs.map(i => genPriceChangeMapFrom(i, 2000));
+
+let allChangesUID = [];
+for (let i = -9; i <= 9; i++)
+    for (let j = -9; j <= 9; j++)
+        for (let k = -9; k <= 9; k++)
+            for (let l = -9; l <= 9; l++) {
+                allChangesUID.push({
+                    uid: getChangesUID([i, j, k, l]),
+                    0: i,
+                    1: j,
+                    2: k,
+                    3: l
+                });
+            }
+
+let maxBananasRecieved = allChangesUID.map(changeUID => {
+    return { 
+        ...changeUID,
+        total: priceChangeMaps.reduce((x, y) => {
+            return x + (y.get(changeUID.uid) ?? 0);
+        }, 0)
+    };
+}).reduce((max, o) => o.total > max ? o.total : max, -1);
 
 console.log(secrets2000.reduce((x, y) => x + y));
+console.log(maxBananasRecieved);
--- AdventOfCode24Day22/secrets.ts
+++ AdventOfCode24Day22/secrets.ts
@@ -1,21 +1,63 @@
-export function getSecretFrom(original: number, n: number) : number {
+import { CantorNegtivePairing, CantorPairing } from "../general/CantorPairing";
+
+export function genSecretFrom(original: number, n: number): number {
     let secret = BigInt(original);
-    for (let i = 0; i < n; i++) {
-        secret = mixAndPrune(secret * 64n, secret);
-        secret = mixAndPrune(secret / 32n, secret);
-        secret = mixAndPrune(secret * 2048n, secret);
-    }
+    for (let i = 0; i < n; i++)
+        secret = genNextSecret(secret);
     return Number(secret);
 }
 
-function mixAndPrune(x: bigint, y: bigint) : bigint {
+export function genPriceChangeMapFrom(original: number, n: number): Map<number, number> {
+    let secret = BigInt(original);
+    let price = getPrice(secret);
+    let changes: [number, number, number, number] = [0, 0, 0, 0];
+    for (let i = 0; i < 4; i++) {
+        secret = genNextSecret(secret);
+        let newPrice = getPrice(secret);
+        changes[i] = newPrice - price;
+        price = newPrice;
+        n--;
+    }
+    let map = new Map<number, number>([[getChangesUID(changes), price]]);
+    for (let i = 0; i < n; i++) {
+        let newSecret = genNextSecret(secret);
+        let newPrice = getPrice(newSecret);
+        changes.shift();
+        changes.push(newPrice - price);
+        secret = newSecret;
+        price = newPrice;
+        let changeUID = getChangesUID(changes);
+        if (!map.has(changeUID))
+            map.set(changeUID, price);
+    }
+    return map;
+}
+
+function getPrice(secret: bigint): number {
+    return Number(secret % 10n);
+}
+
+export function getChangesUID(changes: [number, number, number, number]): number {
+    return CantorPairing(
+        CantorNegtivePairing(changes[0], changes[1]),
+        CantorNegtivePairing(changes[2], changes[3]));
+}
+
+function genNextSecret(secret: bigint): bigint {
+    secret = mixAndPrune(secret * 64n, secret);
+    secret = mixAndPrune(secret / 32n, secret);
+    secret = mixAndPrune(secret * 2048n, secret);
+    return secret;
+}
+
+function mixAndPrune(x: bigint, y: bigint): bigint {
     return prune(mix(x, y));
 }
 
-function mix(x: bigint, y: bigint) : bigint { 
+function mix(x: bigint, y: bigint): bigint {
     return x ^ y;
 }
 
-function prune(x: bigint) : bigint {
+function prune(x: bigint): bigint {
     return x % 16777216n;
 }
