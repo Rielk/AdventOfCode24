89996b31cfa4f02010424e18a8f0e3e79238cf03
Rielk <william@symbotics.co.uk>
2024-12-23 19:03:49 +0000

    Day 23 - Cleanup

--- general/BronKerbosch.ts
+++ general/BronKerbosch.ts
@@ -1,17 +1,16 @@
-export function BronKerbosch(vertices: Iterable<string>, N: (v: string) => Set<string>): Set<string>[] {
+export function BronKerbosch(vertices: Iterable<string>, getNeighbours: (v: string) => Set<string>): Set<string>[] {
     let maximalSets: Set<string>[] = [];
-    return InternalBronKerbosch(new Set(), new Set(vertices), new Set());
+    InternalBronKerbosch(new Set(), new Set(vertices), new Set());
+    return maximalSets;
 
-    function InternalBronKerbosch(R: Set<string>, P: Set<string>, X: Set<string>): Set<string>[] {
-        if (P.size <= 0 && X.size <= 0) {
+    function InternalBronKerbosch(R: Set<string>, P: Set<string>, X: Set<string>): void {
+        if (P.size <= 0 && X.size <= 0)
             maximalSets.push(new Set(R));
-            return maximalSets;
-        }
-        for (let v of P) {
-            InternalBronKerbosch(R.union(new Set([v])), P.intersection(N(v)), X.intersection(N(v)));
-            P.delete(v);
-            X.add(v);
-        }
-        return maximalSets;
+        else
+            for (let v of P) {
+                const neighbours = getNeighbours(v);
+                InternalBronKerbosch(R.add(v), P.intersection(neighbours), X.intersection(neighbours));
+                R.delete(v); P.delete(v); X.add(v);
+            }
     }
 }
