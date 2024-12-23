3fc2cba722f37c6ec8a7a02530759f272c24f45e
Rielk <william@symbotics.co.uk>
2024-12-23 14:39:21 +0000

    Day 23 - Part 1

--- AdventOfCode24Day23/run.ts
+++ AdventOfCode24Day23/run.ts
@@ -1,5 +1,36 @@
-import { getInput } from "../inputs/getInput";
+import { processInput } from "../inputs/getInput";
 
 var example = false;
 
-var data = getInput(23, example);
+var connections = new Map<string, Set<string>>();
+processInput(line => {
+    let [a, b] = line.split('-');
+    safeAdd(a, b);
+    safeAdd(b, a);
+
+    function safeAdd(k: string, v: string) {
+        let set = connections.get(k);
+        if (set == undefined)
+            connections.set(k, set = new Set());
+        set.add(v);
+    }
+}, 23, example);
+
+function find3Networks() {
+    let networks: { 1: string, 2: string, 3: string }[] = [];
+    for (let [computer1, connections1] of connections.entries()) {
+        for (let computer2 of connections1) {
+            let connections2 = connections.get(computer2) ?? new Set();
+            let jointConnections = connections1.intersection(connections2);
+            for (let computer3 of jointConnections)
+                if (computer1 > computer2 && computer2 > computer3)
+                    networks.push({ 1: computer1, 2: computer2, 3: computer3 });
+        }
+    }
+    return networks;
+}
+
+let newtorks = find3Networks();
+let networksWithT = newtorks.filter(n => n[1][0] == 't' || n[2][0] == 't' || n[3][0] == 't');
+
+console.log(networksWithT.length);
